b9f70701fab60f15fb3545c51f78ea60daade527
Jatin <jatin.panjwani@leapfinance.com>
2023-11-26 20:45:49 +0530

    setup typography and colors

---
+++ starter-code/tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "#24053E",
+        "eucaplyptus": "#44FFA1",
+        "davys-grey": "#584D62",
+        "ghost-white": "#FCF8FF"
+      },
+      fontFamily: {
+        sans: ['Manrope', ...defaultTheme.fontFamily.sans],
+        "header": ['Fraunces'],
+      },
+    },
+  },
+  plugins: [],
+}
+
